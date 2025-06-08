44da7239cbfe009a275824518c4f7ab9ee7994df
ShabeebaBanu <banumrmrs.21@itfac.mrt.ac.lk>
2025-06-08 23:44:31 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+ return (
+    <div style={{ textAlign: 'center', fontSize: '2rem', marginTop: '20vh' }}>
+      Hela Helpers
+    </div>
+  );
+}
+
+export default App
